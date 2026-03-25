2a8d7c45f2122ad4dd5e0d9b3a99b766f36d5a24
Suyash <suyashsingh.raebareli@gmail.com>
2026-03-26 04:58:43 +0530

    fixing

--- apps/execution/server.js
+++ apps/execution/server.js
@@ -4,7 +4,7 @@ import {
 } from "./auth.js"
 import { executeRebalance, executeWithdrawal } from "./execute.js"
 
-const VERSION = "1.2.0"
+const VERSION = "1.3.0"
 const REQUEST_TTL_SECONDS = Number(process.env.INTERNAL_REQUEST_TTL_SECONDS || 300)
 const EXECUTION_TIMEOUT_MS = Number(process.env.EXECUTION_TIMEOUT_MS || 60000)
 const recentNonces = new Map()
